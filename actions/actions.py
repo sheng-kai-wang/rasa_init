efe302df05084b2fd96bef469d52ef0a7d0167ff
Sheng Kai Wang <06210173@me.mcu.edu.tw>
2021-09-05 00:32:55 +0800

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -4,7 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
@@ -19,6 +18,7 @@ import json
 import requests
 import http.client
 
+
 def readBotenConfig():
     with open("botenConfig.json", "r") as f:
         data = json.load(f)
@@ -29,23 +29,23 @@ data = readBotenConfig()
 intent = ""
 parameters_array = []
 
+
 class SetIntentAction(Action):
     def name(self) -> Text:
         return "action_set_intent"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        global intent 
+        global intent
         intent = tracker.latest_message['intent'].get('name').split("_", 2)[2]
         return []
+
 
 class DefaultParametersAction(Action):
     def name(self) -> Text:
         return "action_set_default"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         slots = []
         for parameter in data['default']:
@@ -54,79 +54,86 @@ class DefaultParametersAction(Action):
             slots.append(SlotSet(parameter, data['default'][parameter]))
         return slots
 
+
 class AutoLocationAction(Action):
     def name(self) -> Text:
         return "action_auto_location"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         slots = []
-        location = str(tracker.get_slot('auto_latitude')) + ',' + str(tracker.get_slot('auto_longitude'))
+        location = str(tracker.get_slot('auto_latitude')) + ',' + str(
+            tracker.get_slot('auto_longitude'))
         latitude = str(tracker.get_slot('auto_latitude'))
         longitude = str(tracker.get_slot('auto_longitude'))
-        if(data['auto']['latitude,longitude']!=""):
+        if (data['auto']['latitude,longitude'] != ""):
             slots.append(SlotSet(data['auto']['latitude,longitude'], location))
-        if(data['auto']['latitude']!=""):
-            slots.append(SlotSet(data['auto']['latitude'], latitude))       
-        if(data['auto']['longitude']!=""):
+        if (data['auto']['latitude'] != ""):
+            slots.append(SlotSet(data['auto']['latitude'], latitude))
+        if (data['auto']['longitude'] != ""):
             slots.append(SlotSet(data['auto']['longitude'], longitude))
         print('latitude is ' + latitude + ', longitude is ' + longitude)
         # send utter default response to user
-        dispatcher.utter_message(text='Your latitude is ' + latitude + ', longitude is ' + longitude)
+        dispatcher.utter_message(text='Your latitude is ' + latitude +
+                                 ', longitude is ' + longitude)
         return slots
+
 
 class FindSoltAction(Action):
     def name(self) -> Text:
         return "action_slots_values"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         global intent
         output = ''
         try:
-            intent = tracker.latest_message['intent'].get('name').split("_", 2)[2]
+            intent = tracker.latest_message['intent'].get('name').split(
+                "_", 2)[2]
             for parameters in data[intent]['parameters']:
-                if(tracker.get_slot(parameters['name']) != None):
+                if (tracker.get_slot(parameters['name']) != None):
                     output += '- ' + parameters['name'] + ': ' + \
                         tracker.get_slot(parameters['name']) + '\n'
         except Exception as e:
             print(e)
         # send utter default response to user
         dispatcher.utter_message(text=output)
-        return[]
+        return []
 
 
 class ApiAction(Action):
     def name(self) -> Text:
         return "action_use_api"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         global intent
         slots_dict = dict()
         try:
-            intent = tracker.latest_message['intent'].get('name').split("_", 2)[2]
+            intent = tracker.latest_message['intent'].get('name').split(
+                "_", 2)[2]
         except Exception as e:
             print(e)
         # Processing flow
-        if(data[intent].__contains__('flow')):
+        if (data[intent].__contains__('flow')):
             for flow in data[intent]['flow']:
                 # Processing getSlots and get requests
                 try:
                     for getSlots in flow['getSlots']:
-                        if(getSlots['type'] == 'Single'):
+                        if (getSlots['type'] == 'Single'):
                             resp = myRequests(
-                                tracker, flow['intent'], getSlots['parameterName'], slots_dict[getSlots['parameterName']][0])
+                                tracker, flow['intent'],
+                                getSlots['parameterName'],
+                                slots_dict[getSlots['parameterName']][0])
                             utter_text = determinalStatusCode(
                                 resp, flow['intent'])
                             dispatcher.utter_message(text=utter_text)
                         else:
-                            for slot_value in slots_dict[getSlots['parameterName']]:
-                                resp = myRequests(
-                                    tracker, flow['intent'], getSlots['parameterName'], slot_value)
+                            for slot_value in slots_dict[
+                                    getSlots['parameterName']]:
+                                resp = myRequests(tracker, flow['intent'],
+                                                  getSlots['parameterName'],
+                                                  slot_value)
                                 utter_text = determinalStatusCode(
                                     resp, flow['intent'])
                                 dispatcher.utter_message(text=utter_text)
@@ -141,10 +148,12 @@ class ApiAction(Action):
                     for responseToSlots in flow['responseToSlots']:
                         jsonpath_expression = parse(
                             responseToSlots['jsonPath'])
-                        emp_ids_list = [match.value for match in jsonpath_expression.find(
-                            json.loads(resp.text))]
-                        slots_dict[responseToSlots['parameterName']
-                                   ] = emp_ids_list
+                        emp_ids_list = [
+                            match.value for match in jsonpath_expression.find(
+                                json.loads(resp.text))
+                        ]
+                        slots_dict[
+                            responseToSlots['parameterName']] = emp_ids_list
                         print(slots_dict)
                 except Exception as e:
                     print(e)
@@ -154,44 +163,53 @@ class ApiAction(Action):
             resp = myRequests(tracker, intent, None, None)
             utter_text = determinalStatusCode(resp, intent)
             dispatcher.utter_message(text=utter_text)
-        return[]
+        return []
 
 
 def myRequests(tracker, intent, slot, slot_value):
     params = dict()
+    headers = dict()
     url = data['url'] + data[intent]['pathName']
     # Processing parameters to params
     try:
         for parameters in data[intent]['parameters']:
-            if(parameters['name'] != slot):
-                if(parameters['in'] == 'path'):
+            if (parameters['name'] != slot):
+                if (parameters['in'] == 'path'):
                     try:
-                        url = data['url'] + data[intent]['pathName'].split("{")[0] + tracker.get_slot(
-                            parameters['name']) + data[intent]['pathName'].split("{")[1].split("}")[1]
+                        url = data['url'] + data[intent]['pathName'].split(
+                            "{")[0] + tracker.get_slot(
+                                parameters['name']) + data[intent][
+                                    'pathName'].split("{")[1].split("}")[1]
                     except:
                         print("not has path params")
-                elif(parameters['in'] == 'query'):
+                elif (parameters['in'] == 'query'):
                     params[parameters['name']] = tracker.get_slot(
+                        parameters['name'])
+                elif (parameters['in'] == 'header'):
+                    headers[parameters['name']] = tracker.get_slot(
                         parameters['name'])
             # Processing response to slot
             else:
-                if(parameters['in'] == 'path'):
+                if (parameters['in'] == 'path'):
                     try:
                         url = data['url'] + data[intent]['pathName'].split(
-                            "{")[0] + str(slot_value) + data[intent]['pathName'].split("{")[1].split("}")[1]
+                            "{")[0] + str(slot_value) + data[intent][
+                                'pathName'].split("{")[1].split("}")[1]
                     except:
                         print("not has path params")
-                elif(parameters['in'] == 'query'):
+                elif (parameters['in'] == 'query'):
                     params[parameters['name']] = slot_value
+                elif (parameters['in'] == 'header'):
+                    headers[parameters['name']] = slot_value
     except KeyError:
         print(data[intent] + "'s parameters is unknown.")
     print(url)
-    return requests.get(url=url, params=params)
+    return requests.get(url=url, params=params, headers=headers)
 
 
 def determinalStatusCode(resp, intent):
     # Determine whether the status code is OK
-    if(resp.ok):
+    if (resp.ok):
         resp_data = json.loads(resp.text)
         print(resp_data)
         return parseJsonPath(intent, resp_data)
@@ -200,8 +218,8 @@ def determinalStatusCode(resp, intent):
         print(resp.status_code)
         status_message['message'] = str(
             resp.status_code) + ": " + http.client.responses[resp.status_code]
-        json_string = json.dumps(
-            status_message, ensure_ascii=False).encode('utf8')
+        json_string = json.dumps(status_message,
+                                 ensure_ascii=False).encode('utf8')
         return json_string.decode()
 
 
@@ -222,11 +240,11 @@ def parseJsonPath(intent, res):
     json_string = json.dumps(reslut, ensure_ascii=False).encode('utf8')
     return json_string.decode()
 
+
 class SlotReset(Action):
     def name(self) -> Text:
         return "action_reset_slot"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         return [AllSlotsReset()]
